a460bb18817c3830695b20587ace5c53237c1414
Thomas <digitalwizard@comcast.net>
2020-04-09 21:14:20 -0500

    Added two CPP classes. Tank.cpp/h and TankPlayerController.cpp/h. Made them parents of the BPs.

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "Tank.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+// Call the BeginPlay() function
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController BeginPlay"));
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessed Tank not found."));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Possessed Tank: %s"), *ControlledTank->GetName());
+	}
+	
+}
